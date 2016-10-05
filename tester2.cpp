a6f7d84c02180af04d0ff188f38530b03ddb414f
atishay197 <atishay197@gmail.com>
2016-10-05 12:58:12 +0530

    mapWriter working for basic cases

--- tester2.cpp
+++ tester2.cpp
@@ -1,9 +1,9 @@
 #include <bits/stdc++.h>
 
-int b[243][5];
-
+// TO DO Remove global variable
+float b[243][6];
 // TO DO : Convert to recusive or maybe not
-void permute(int a[5][3])
+void permute(int a[5][3], bool increaserMode)
 {
 	int i,j,k,l,m,n=0;
 	for(i=0 ; i<3 ; i++)
@@ -12,37 +12,81 @@ void permute(int a[5][3])
 				for(l=0 ; l<3 ; l++)
 					for(m=0 ; m<3 ; m++)
 					{
+						if(i==1 && j==1 && k==1 && l==1 && m==1)
+							continue;
 						b[n][0] = a[0][i];
 						b[n][1] = a[1][j];
 						b[n][2] = a[2][k];
 						b[n][3] = a[3][l];
 						b[n][4] = a[4][m];
-						for(int p=0 ; p<5 ; p++)
-							printf("%d\t",b[n][p]);
-						printf("\n");
+						b[n][5] = 1;
+						// for(int p=0 ; p<5 ; p++)
+						// 	printf("%d\t",b[n][p]);
+						// printf("\n");
+						if(increaserMode)
+						{
+							if(i != 1)
+								b[n][5] /= 2;
+							if(j != 1)
+								b[n][5] /= 2;
+							if(k != 1)
+								b[n][5] /= 2;
+							if(l != 1)
+								b[n][5] /= 2;
+							if(m != 1)
+								b[n][5] /= 2;
+						}
 						n++;						
 					}
 }
 
-float getFromMap(int dest[5])
+float getFromMap(float dest[5])
 {
-	float val = 0;
+	float val = 80;
 	for(int i=0 ; i<5 ; i++)
-		val += dest[i];
+		val -= 0.1*dest[i];
 	return val;
 }
 
-bool inRange(int x)
+bool inRange(float x[5])
 {
-	if(x < 0 || x > 9)
-		return false;
+	for(int i=0 ; i<5 ; i++)
+		if(x[i] < 0 || x[i] > 9)
+			return false;
 	return true;
 }
 
-float mapModifier5D(int dest[5],float increase)
+void mapincementer5D(float dest[5],float increase)
 {
-	int i;
-	printf("Map : %f\n",getFromMap(dest));
+	int i,j;
+	int calcDiff[5][3] = {0};
+	for(i=0 ; i<5 ; i++)
+	{
+		calcDiff[i][0] = dest[i]-1;
+		calcDiff[i][1] = dest[i];
+		calcDiff[i][2] = dest[i]+1;
+	}
+	for(i=0 ; i<242 ; i++)
+		for(j=0 ; j<5 ; j++)
+			b[i][j] = 10;
+	permute(calcDiff,1);
+	for(i=0 ; i<5 ; i++)
+		b[242][i] = dest[i];
+	b[242][5] = increase;
+	for(i=0 ; i<242 ; i++)
+		b[i][5] *= increase;
+	for(i=0 ; i<243 ; i++)
+	{
+		for(j=0 ; j<6 ; j++)
+			printf("%f ",b[i][j]);
+		printf("\n");
+	}
+}
+
+float findincreaseLimit5D(float dest[5],float increase)
+{
+	int i,j;
+	// printf("Map : %f\n",getFromMap(dest));
 	int calcDiff[5][3] = {0};
 	for(i=0 ; i<5 ; i++)
 	{
@@ -50,48 +94,49 @@ float mapModifier5D(int dest[5],float increase)
 		calcDiff[i][1] = dest[i];
 		calcDiff[i][2] = dest[i]+2;
 	}
+	for(i=0 ; i<242 ; i++)
+		for(j=0 ; j<242 ; j++)
+			b[i][j] = 10;
 	// for(i=0 ; i<5 ; i++)
 	// 	printf("%d %d %d\n",calcDiff[0][i],calcDiff[1][i],calcDiff[2][i]);
-	permute(calcDiff);
-	// To be used when advanced usage with bigger map and sinosoudal map increment is required
-	// float scaleMultiplier[5][2] = {0};
-	// for(i=0 ; i<5 ; i++)
-	// {
-	//  	scaleMultiplier[i][0] = 0.1*(getFromMap(dest) - getFromMap(calcDiff[i][0]));
-	//  	scaleMultiplier[i][1] = 0.1*(getFromMap(dest) - getFromMap(calcDiff[i][1]));
-	// }
-	// Use linear equations as number of points are reduced
-	for(i=0 ; i<5 ; i++)
+	permute(calcDiff,0);
+	bool mode = increase>0?1:0;
+	float finalMax = 101, finalMin = -1;
+	float curMapValue = getFromMap(dest);
+	float increaseedValue = curMapValue + increase;
+	for(i=0 ; i<242 ; i++)
 	{
-		if(inRange(calcDiff[i][0]))
-			lRange[0] = getFromMap(calcDiff[0]);
-		// else if(inRange(calcDiff[i][0]-1))
-		// 	lRange[i] = getFromMap(calcDiff[i][0]-1);
-		else
-			lRange[0] = 102;
-		if(inRange(calcDiff[i][1]))
-			rRange[0] = getFromMap(calcDiff[1]);
-		// if(inRange(calcDiff[i][1]-1))
-		// 	rRange[i] = getFromMap(calcDiff[i][1]-1);
-		else
-			rRange[0] = 102;
+		if(inRange(b[i]))
+		{
+			float range = getFromMap(b[i]);
+			if(range<finalMax)
+				finalMax = range;
+			if(range>finalMin)
+				finalMin = range;
+		}
 	}
-	float finalIncrease;
-	for(i=0 ; i<5 ; i++)
-	{
-		printf("L : %f\tR : %f\n",lRange[i],rRange[i]);
-		float curFinal = lRange[i]<rRange[i]?lRange[i]:rRange[i];
-		if(curFinal < finalIncrease)
-			finalIncrease = curFinal;
-	}
-	printf("Final : %f\n",finalIncrease);
-	if(increase < finalIncrease)
-		finalIncrease = increase;
-	return 0;
+	printf("Cur : %f\tMax : %f\tMin : %f\n",curMapValue,finalMax,finalMin);
+	float percentageInRange = 0.05; // TO DO 5% is arbitrary vary value of you want to 
+	finalMax *= 1+percentageInRange ;
+	finalMin /= 1+percentageInRange ;
+	if(finalMin < 0)
+		finalMin = 0;
+	if(finalMax > 99.99)
+		finalMax = 99.99;
+	printf("Inc : %f\tMax : %f\tMin : %f\n", increaseedValue,finalMax,finalMin);
+	if(!(increaseedValue < finalMax && increaseedValue > finalMin))
+		if(mode)
+			increase = finalMax - curMapValue;
+		else
+			increase = finalMin - curMapValue;
+	printf("Final : %f\n",increase);
+	mapincementer5D(dest,increase);
+	return increase;
 }
 
 int main()
 {
-	int dest[5] = {3,4,5,6,7};
-	mapModifier5D(dest,3);
+	float dest[5] = {3,4,5,6,7};
+	float increase = -34;
+	increase = findincreaseLimit5D(dest,increase);
 }
